1b792125644f5749c8d374f7536d1de5f1f8ed43
Kleis <Mathew.Kleisinger@gmail.com>
2022-04-18 00:19:39 -0500

    Cloned from my original group repo.

---
+++ finalprojectSD340/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using finalprojectSD340.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace finalprojectSD340.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+    }
+}
